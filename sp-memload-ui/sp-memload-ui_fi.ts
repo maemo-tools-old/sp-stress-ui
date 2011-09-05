bd6043914cc0aa9669de32ee995d340db6694917
Tommi Rantala <ext-tommi.1.rantala@nokia.com>
2011-09-05 14:38:07 +0300

    sp-memload-ui: update finnish translation

--- sp-memload-ui/sp-memload-ui_fi.ts
+++ sp-memload-ui/sp-memload-ui_fi.ts
@@ -4,106 +4,101 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="MainPage.qml" line="33"/>
-        <source>&lt;h1&gt;sp-memload&lt;/h1&gt;</source>
-        <translation>&lt;h1&gt;sp-memload&lt;/h1&gt;</translation>
-    </message>
-    <message>
-        <location filename="MainPage.qml" line="36"/>
-        <source>&lt;h2&gt;Memory Allocation&lt;/h2&gt;</source>
-        <translation>&lt;h2&gt;Muistivaraus&lt;/h2&gt;</translation>
-    </message>
-    <message>
-        <location filename="MainPage.qml" line="42"/>
-        <source>&lt;h4&gt;Size:&lt;/h4&gt;</source>
-        <translation>&lt;h4&gt;Koko:&lt;/h4&gt;</translation>
-    </message>
-    <message>
-        <location filename="MainPage.qml" line="48"/>
+        <location filename="MainPage.qml" line="126"/>
         <source>Megabytes</source>
         <translation>Megatavua</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="60"/>
-        <source>&lt;h4&gt;Fill:&lt;/h4&gt;</source>
-        <translation>&lt;h4&gt;Täyttö:&lt;/h4&gt;</translation>
+        <location filename="MainPage.qml" line="109"/>
+        <source>Memory Allocation</source>
+        <translation>Muistivaraus</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="71"/>
+        <location filename="MainPage.qml" line="119"/>
+        <source>Size:</source>
+        <translation>Koko:</translation>
+    </message>
+    <message>
+        <location filename="MainPage.qml" line="138"/>
+        <source>Fill:</source>
+        <translation>Täyttö:</translation>
+    </message>
+    <message>
+        <location filename="MainPage.qml" line="150"/>
         <source>0x00</source>
         <translation>0x00</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="79"/>
+        <location filename="MainPage.qml" line="158"/>
         <source>Randomized</source>
         <translation>Satunnaistettu</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="93"/>
+        <location filename="MainPage.qml" line="172"/>
         <source>Re-read</source>
         <translation>Lue</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="98"/>
+        <location filename="MainPage.qml" line="177"/>
         <source>Rewrite</source>
         <translation>Kirjoita</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="106"/>
+        <location filename="MainPage.qml" line="185"/>
         <source>Free</source>
         <translation>Vapauta</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="112"/>
+        <location filename="MainPage.qml" line="191"/>
         <source>Allocate</source>
         <translation>Varaa</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="119"/>
-        <source>&lt;font color=&apos;red&apos;&gt;Allocation failed!&lt;/font&gt;</source>
-        <translation>&lt;font color=&apos;red&apos;&gt;Muistivaraus epäonnistui!&lt;/font&gt;</translation>
+        <location filename="MainPage.qml" line="198"/>
+        <source>Allocation failed!</source>
+        <translation>Muistivaraus epäonnistui!</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="129"/>
+        <location filename="MainPage.qml" line="236"/>
+        <source>Control Group</source>
+        <translation>Control Group</translation>
+    </message>
+    <message>
+        <location filename="MainPage.qml" line="264"/>
+        <source>Group statistics</source>
+        <translation>Control Groupin muistinkäyttötiedot</translation>
+    </message>
+    <message>
+        <location filename="MainPage.qml" line="209"/>
         <source>Allocation size is &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt; or &lt;b&gt;%2&amp;nbsp;kB&lt;/b&gt;. Statistics for the virtual memory area that contains the allocation:&lt;pre&gt;%3&lt;/pre&gt;</source>
         <oldsource>Allocation size is &lt;b&gt;%1 MB&lt;/b&gt; or &lt;b&gt;%2 kB&lt;/b&gt;. Statistics for the virtual memory area that contains the allocation:&lt;pre&gt;%3&lt;/pre&gt;</oldsource>
         <translation>Muistivarauksen koko on &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt; tai &lt;b&gt;%2&amp;nbsp;kB&lt;/b&gt;. Lisätietoja muistivarauksen sisältämästä virtuaalimuistialueesta:&lt;pre&gt;%3&lt;/pre&gt;</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="146"/>
+        <location filename="MainPage.qml" line="226"/>
         <source>Update statistics</source>
         <translation>Päivitä</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="156"/>
-        <source>&lt;h2&gt;Control Group&lt;/h2&gt;</source>
-        <translation>&lt;h2&gt;Control Group&lt;/h2&gt;</translation>
-    </message>
-    <message>
-        <location filename="MainPage.qml" line="178"/>
-        <source>&lt;h3&gt;Group statistics&lt;/h3&gt;</source>
-        <translation>&lt;h3&gt;Control Groupin muistinkäyttötiedot&lt;/h3&gt;</translation>
-    </message>
-    <message>
-        <location filename="MainPage.qml" line="182"/>
+        <location filename="MainPage.qml" line="271"/>
         <source>Memory usage: &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt; or &lt;b&gt;%2&amp;nbsp;kB&lt;/b&gt;.</source>
         <oldsource>Memory usage: &lt;b&gt;%1 MB&lt;/b&gt; or &lt;b&gt;%2 kB&lt;/b&gt;.</oldsource>
         <translation>Keskusmuistin käyttö: &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt; tai &lt;b&gt;%2&amp;nbsp;kB&lt;/b&gt;.</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="189"/>
+        <location filename="MainPage.qml" line="278"/>
         <source>Memory limit: &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt; or &lt;b&gt;%2&amp;nbsp;kB&lt;/b&gt; (%3% used).</source>
         <oldsource>Memory limit: &lt;b&gt;%1 MB&lt;/b&gt; or &lt;b&gt;%2 kB&lt;/b&gt; (%3% used).</oldsource>
         <translation>Suurin sallittu keskusmuistin käyttö: &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt; tai &lt;b&gt;%2&amp;nbsp;kB&lt;/b&gt; (käytössä %3%).</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="197"/>
+        <location filename="MainPage.qml" line="286"/>
         <source>Swap usage: &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt;.</source>
         <oldsource>Swap usage: &lt;b&gt;%1 MB&lt;/b&gt;.</oldsource>
         <translation>Heittovaihtotiedoston käyttö: &lt;b&gt;%1&amp;nbsp;MB&lt;/b&gt;.</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="202"/>
+        <location filename="MainPage.qml" line="291"/>
         <source>Update</source>
         <translation>Päivitä</translation>
     </message>
